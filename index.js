77cc2a0cf6c0125b381203b3f999d24aecd5fe26
Aminul Hoque Anik <a01831435209@gmail.com>
2024-04-19 03:29:43 +0600

    'booking-done'

--- index.js
+++ index.js
@@ -26,15 +26,45 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const database = client.db("doctors").collection('admins');
+    const serviceDatabase = client.db("doctors").collection('admins');
+    const bookingDatabase = client.db("doctors").collection('bookings');
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   
     app.get('/services', async(req, res) => {
         const query ={};
-        const cursor = database.find(query);
+        const cursor = serviceDatabase.find(query);
         const data = await cursor.toArray();
         res.send(data)
       })
+
+    app.get('/available',async(req,res)=>{
+      const date = req.query.date;
+
+      const services = await serviceDatabase.find().toArray();
+
+      const quarry={Date:date}
+      const bookings = await bookingDatabase.find(quarry).toArray()
+      services.forEach(service =>{
+        const serviceBookings = bookings.filter(b=>b.Treatment === service.service)
+        const booked = serviceBookings.map(s=> s.SlotId)
+        const available = service.slots.filter(s=> !booked.includes(s.id))
+        service.slots = available
+        
+      })
+      res.send(services)
+    })
+
+
+    app.post('/bookings', async(req,res)=>{
+      const data = req.body
+      const quarry = {Treatment: data.Treatment,Date:data.Date,Name:data.Name}
+      const exist = await bookingDatabase.findOne(quarry)
+      if(exist){
+        return res.send({success : false})
+      }
+      const result = await bookingDatabase.insertOne(data)
+      return res.send({success: true ,result})
+    })
 
 
 } finally {
